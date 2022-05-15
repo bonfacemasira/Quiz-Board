0882f178dc17f22f6d5eebcfc6d40347a579bae0
bonfacemasira <bonfacemasira@gmail.com>
2022-05-15 11:53:51 +0300

    add questions to display on JS

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+const theQuestions = [
+    {
+        question:"1. What is the short form of HyperText Markup Language?",
+        a: "HTML",
+        b:"HMLT",
+        c:"MLHT",
+        correct: "a",
+    },
+
+    {
+        question:"2. What is the other name for Javascript?",
+        a: "ECMAscript",
+        b:"JS",
+        c:"I don't know",
+        correct: "b",
+    },
+
+    {
+        question:"3. Is C++ a low level language?",
+        a: "Yes",
+        b:"No",
+        correct: "b",
+    },
+
+    {
+        question:"4. How do you call a function named myFunction?",
+        a: "myFunction()",
+        b:"call myFunction()",
+        c:"call function myFunction()",
+        correct: "a",
+    },
+
+    {
+        question:"5. How do you create a function in JavaScript?",
+        a: "function myFunction()",
+        b:"function=myFunction()",
+        c:"function:myFunction()",
+        correct: "a",
+    }
+];
+
+const quiz = document.getElementById('questions_section')
+const answer
