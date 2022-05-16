3583c929162e5a597ac8bd3df80e638c66c66d81
bonfacemasira <bonfacemasira@gmail.com>
2022-05-16 10:57:14 +0300

    delete questions.js and add functionality to submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,76 +1,31 @@
-const theQuestions = [
-    {
-        question:"1. What is the short form of HyperText Markup Language?",
-        a: "HTML",
-        b:"HMLT",
-        c:"MLHT",
-        correct: "a",
-    },
+let questions = document.getElementsByClassName('question');
 
-    {
-        question:"2. What is the other name for Javascript?",
-        a: "ECMAscript",
-        b:"JS",
-        c:"I don't know",
-        correct: "b",
-    },
+ 
+function submit(){
 
-    {
-        question:"3. Is C++ a low level language?",
-        a: "Yes",
-        b:"No",
-        correct: "b",
-    },
+    let results  = 0;
+    if (document.getElementById('cr1').checked){
+     results +=20;
 
-    {
-        question:"4. How do you call a function named myFunction?",
-        a: "myFunction()",
-        b:"call myFunction()",
-        c:"call function myFunction()",
-        correct: "a",
-    },
-
-    {
-        question:"5. How do you create a function in JavaScript?",
-        a: "function myFunction()",
-        b:"function=myFunction()",
-        c:"function:myFunction()",
-        correct: "a",
     }
-];
+    
+    if (document.getElementById('cr2').checked){
+        results +=20;
+       }
+    
+    if (document.getElementById('cr3').checked){
+        results +=20;
+   
+       }
+    
+    if (document.getElementById('cr4').checked){
+        results +=20;
+       }
+    
+    if (document.getElementById('cr5').checked){
+        results +=20;
+       }
 
-const start =document.getElementsByClassName('start');
-const quiz = document.getElementsByClassName('questions_section');
-const submit = document.getElementsByClassName('submit');
-const results = document.getElementsByClassName('results');
-
-
-// Start
-
-start.addEventListener('click', displayQuiz);
-
-
-// Create function for building the Quiz
-function displayQuiz (){
-
-    const output =[];
-
-    theQuestions.forEach(
-        (currentQusetion) => {
-            const answers = [];
-
-            for(letter in )
-        }
-    )
+    alert(results);
+    
 }
-
-//Create function for showing the results
-function displayResults(){}
-
-//Display Quiz Immediately
-displayQuiz();
-
-
-submit.addEventListener('click', displayResults);
-
-
